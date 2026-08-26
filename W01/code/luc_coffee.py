ff83a6135efbbe21379f387be2fc25a9e5eff076
Kennaoui <kennaoui@luc.edu>
2026-08-26 16:44:54 +0000

    Live Code W01 Lecture 2

---
+++ W01/code/luc_coffee.py
@@ -0,0 +1,15 @@
+customer = "Leo"
+
+order = "Tea"
+
+print("Welcome", customer, "to LUC Coffee!")
+
+print("Your order is:", order)
+
+price = "three"
+
+print("Price:", price, "$")
+
+total = price * 1.11
+
+print("Total price:", total)
