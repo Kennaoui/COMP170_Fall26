b2469457bc3a7672e48322a15e34d83d0cdaa5dd
Kennaoui <kennaoui@luc.edu>
2026-09-11 09:08:06 -0500

    Create cookie_baking_steps.py

---
+++ W03/cookie_baking_steps.py
@@ -0,0 +1,17 @@
+print("Mix the ingredients.") 
+print("Shape the cookies.")  
+
+print("Preheat the oven to 350 degrees.") 
+print("Place one tray in the oven.") 
+print("Set the timer for 12 minutes.") 
+print("Remove the tray.")  
+
+print("Preheat the oven to 350 degrees.") 
+print("Place one tray in the oven.") 
+print("Set the timer for 12 minutes.") 
+print("Remove the tray.")
+
+print("Preheat the oven to 350 degrees.") 
+print("Place one tray in the oven.") 
+print("Set the timer for 12 minutes.") 
+print("Remove the tray.")
