4a259fb898ef03a9a25b0a53fcbce18da3151c5a
Kennaoui <kennaoui@luc.edu>
2026-09-23 12:02:39 -0500

    Modify get_bmr_input to accept user index

--- W5/bmr.py
+++ W5/bmr.py
@@ -1,16 +1,16 @@
-def get_bmr_input():
+def get_bmr_input(i : int):
     """
-    Get user input for BMR calculation.
+    Get input from user number i for BMR calculation.
     Returns:
-        weight (float): Weight in kilograms.
-        height (float): Height in centimeters.
+        weight (float): Weight in pounds.
+        height (float): Height in inches.
         age (int): Age in years.
-        gender (str): Gender ('male' or 'female').
+        gender (int): Gender (0 for 'male' or 1 for 'female').
     """
-    weight = float(input("Enter your weight in kilograms: "))
-    height = float(input("Enter your height in centimeters: "))
-    age = int(input("Enter your age in years: "))
+    weight = float(input("Enter weight in pounds of user #" + i + " :"))
+    height = float(input("Enter height in inches of user #" + i + " :"))
+    age = int(input("Enter age in years of user #" + i + " :"))
     #gender = input("Enter your gender (male/female): ").strip().lower()
-    gender = int(input("Enter  0 if male, 1 if female: "))
+    gender = int(input("Enter  0  if user #" + i + " is male, 1 otherwise:"))
     
     return weight, height, age, gender
