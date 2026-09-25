9dc67cf79b685de673f9fd7593b839f48639ef8f
Kennaoui <kennaoui@luc.edu>
2026-09-25 09:50:22 -0500

    Create garden_math.py

---
+++ W5/garden_math.py
@@ -0,0 +1,11 @@
+
+def side_length(area: float) -> float:
+    """Return the side length of a square with the given area."""
+    # TODO: Use a function from the math module.
+    pass
+
+
+def whole_side_length(area: float) -> int:
+    """Return the smallest whole-number side length that is large enough."""
+    # TODO: Use functions from the math module.
+    pass
