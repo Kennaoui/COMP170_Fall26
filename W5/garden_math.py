c3e72f6afe211dac419257c834b3e4d3d800f370
Kennaoui <kennaoui@luc.edu>
2026-09-25 11:19:22 -0500

    Update garden_math.py

--- W5/garden_math.py
+++ W5/garden_math.py
@@ -1,11 +1,11 @@
+from math import sqrt
 
 def side_length(area: float) -> float:
     """Return the side length of a square with the given area."""
-    # TODO: Use a function from the math module.
-    pass
+    return sqrt(area)
 
 
 def whole_side_length(area: float) -> int:
-    """Return the smallest whole-number side length that is large enough."""
+    """Return the biggest whole-number side length that is large enough."""
     # TODO: Use functions from the math module.
     pass
