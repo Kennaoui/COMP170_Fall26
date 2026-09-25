37b745a486bccb76c5ec4c78d3e426ad8626b3ba
Kennaoui <kennaoui@luc.edu>
2026-09-25 10:02:58 -0500

    Change loop from while to for for garden processing

---
+++ W5/garden_planner.py
@@ -0,0 +1,18 @@
+
+def main() -> None:
+    """Ask for garden areas and display their side lengths."""
+  
+    number_of_gardens = int(input("How many gardens? "))
+
+    # TODO: Set up a counter.
+
+    # TODO: Use a for loop to process gardens.
+    # Inside the loop:
+    # 1. Ask for the garden's area.
+    # 2. If the area is negative, print an error message.
+    # 3. Otherwise, call both functions from garden_math
+    #    and print their results.
+    pass
+
+
+main()
